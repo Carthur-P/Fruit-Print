bb72dac311183d5733bc5d0700ed39ff2d5f0fda
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-07-07 15:57:33 +1200

    adding home screen animation

---
+++ app.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+    $(".homeContent").hide().fadeIn(1100);
+    $(".backgroundFruit").animate({
+        left: "60%"
+    }, 1000, "linear").css({"transform": "rotate(-100deg)"});
+});
