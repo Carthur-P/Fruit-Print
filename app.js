38ed530c8f55fe549338a6665f2be34d782bf350
pongp1 <pongp1@student.op.ac.nz>
2020-07-08 08:57:07 +1200

    adding contact me contents

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
     $(".homeContent").hide().fadeIn(1100);
-    $(".backgroundFruit").animate({
+    $(".rightBigFruit").animate({
         left: "60%"
     }, 1000, "linear").css({"transform": "rotate(-90deg)"});
 });
