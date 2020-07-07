a025729ead5400218a67c742d30e1b43ee79197a
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-07-08 10:00:15 +1200

    adding animation to nav bar

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
     $(".homeContent").hide().fadeIn(1100);
+    $("nav").hide().fadeIn(1100);
     $(".rightBigFruit").animate({
         left: "60%"
     }, 1000, "linear").css({"transform": "rotate(-90deg)"});
