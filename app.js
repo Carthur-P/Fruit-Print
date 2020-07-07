53f32e3398760cfdd229748f3b27d538a6174b7e
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-07-08 07:59:33 +1200

    adding show prints content

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ $(document).ready(() => {
     $(".homeContent").hide().fadeIn(1100);
     $(".backgroundFruit").animate({
         left: "60%"
-    }, 1000, "linear").css({"transform": "rotate(-100deg)"});
+    }, 1000, "linear").css({"transform": "rotate(-90deg)"});
 });
