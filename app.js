d08eab9dd22994919b7b827b5b2547737170167c
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-07-09 11:07:34 +1200

    adding scroll animation

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 $(document).ready(() => {
-    $(".homeContent").hide().fadeIn(1100);
-    $("nav").hide().fadeIn(1100);
-    $(".rightBigFruit").animate({
-        left: "60%"
-    }, 1000, "linear").css({"transform": "rotate(-90deg)"});
+    //scrolling animation
+    const $win = $(window);
+    //get all the pages
+    const scrollContainers = $(".scroll");
+    $win.on("scroll resize", () => {
+        //get the scroll position which will be the size of the screen minus 500px
+        let scrollPosition = $win.scrollTop() + $win.height() - 500;
+        //loop through all the pages and check if scroll position is in the page
+        scrollContainers.each((index, element) => {
+            $element = $(element);
+            //get page top position relative to the document
+            let elementTop = $element.position().top;
+            //get page bottom position relative to the document
+            let elementBottom = $element.position().top + $element.outerHeight();
+            //checking if scroll position is within the page top and bottm position
+            if(scrollPosition >= elementTop && scrollPosition <= elementBottom){
+                //add class that will animate the content inside the page
+                $element.children(".hidden").addClass("fadeIn");
+                $element.children(".leftBigFruit").addClass("slideRight");
+                $element.children(".rightBigFruit").addClass("slideLeft");
+            }
+        });
+    });
+    //triggering onscroll onload aswell
+    $win.trigger("scroll");
 });
